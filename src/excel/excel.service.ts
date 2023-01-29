4af7a113efb1e85046dd06cc3cf53a728ce8ef2b
Hlib Derbenov <50cent5088@gmail.com>
2023-01-29 20:19:39 +0200

    feature(excel): extend excel service with utility functionality

--- src/excel/excel.service.ts
+++ src/excel/excel.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import Excel, { Workbook } from 'exceljs';
+import { createReadStream, ReadStream } from 'fs';
 
 @Injectable()
-export class ExcelService {}
+export class ExcelService {
+  public getCellValue(row: Excel.Row, cellIndex: number) {
+    const cell = row.getCell(cellIndex);
+    return cell.value ? cell.value.toString() : '';
+  }
+
+  public async readFileWithStream(
+    path: string,
+    delimiter = ';',
+    rowCount?: number,
+  ): Promise<Excel.Row[]> {
+    const stream: ReadStream = createReadStream(path);
+
+    const workbook: Excel.Workbook = new Workbook();
+    const streamWorkBook: Excel.Worksheet = await workbook.csv.read(stream, {
+      parserOptions: { delimiter },
+    });
+
+    const rows: Excel.Row[] =
+      streamWorkBook.getRows(2, (rowCount ?? streamWorkBook.rowCount) - 1) ??
+      [];
+
+    return rows;
+  }
+}

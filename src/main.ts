f7ea64e8a141667593b0ad4994d95c4b3321b7f6
Hlib Derbenov <50cent5088@gmail.com>
2023-01-29 20:03:33 +0200

    feature(amenity): add swagger endpoint, security, cors, error factory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      exceptionFactory: (errors) => new BadRequestException(errors),
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const options = new DocumentBuilder()
+    .setTitle('Backend')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
